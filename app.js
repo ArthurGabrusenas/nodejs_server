96514d2bdbffb98744ed45ec48628c607249d06f
gabrusenas.ar <gabrusenas@outlook.com>
2022-06-22 00:09:30 +0300

    WIP

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3100;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const mockDataPath = {
+  users: "./mock-data/users.json",
+};
 
-app.listen(port, () => {
-  console.log("Hello World!");
-});
+const routes = {
+  users: "/users",
+};
+
+function getData(path) {
+  const data = require(path);
+
+  return data;
+}
+
+function putHttpRoute(route, method, mockData) {
+  switch (method) {
+    case "post":
+      app.post(route, (req, res) => {
+        res.send(mockData);
+      });
+      break;
+
+    default:
+      app.get(route, (req, res) => {
+        res.send(mockData);
+      });
+  }
+}
+
+putHttpRoute(routes.users, "post", getData(mockDataPath.users));
+
+app.listen(port);
