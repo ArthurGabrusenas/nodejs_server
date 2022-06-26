6af21764f912efafb9c6c9c62755797fb03d7503
gabrusenas.ar <gabrusenas@outlook.com>
2022-06-26 22:37:31 +0300

    add  request parser, "add product endpoint", "update product endpoint"

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const app = express();
+const multer = require("multer");
 const cors = require("cors");
+const app = express();
 const port = 3100;
+const upload = multer();
+const bodyParser = require("body-parser");
 
 const mockDataPath = {
   users: "./mock-data/users.json",
@@ -9,16 +12,58 @@ const mockDataPath = {
 
 const routes = {
   users: "/users",
-  user: "/user/:id",
+  userProduct: "/user/:userId/products/:productId",
+  userProducts: "/user/:userId/products",
 };
 
+//  helpers //
 function getData(path) {
   const data = require(path);
 
   return data;
 }
+////
 
-function putHttpRoute(app, route, method, mockData = null) {
+function addUserProduct(request) {
+  const appendUserId = Number(request.params.userId);
+  let savedUsers = getData(mockDataPath.users);
+  const addedProduct = {
+    id: savedUsers.users[appendUserId].products.length,
+    ...request.body,
+  };
+
+  const newProductsObject = savedUsers.users.map((user) => {
+    if (appendUserId === user.id) {
+      user.products.push(addedProduct);
+    }
+  });
+
+  // TODO write a newProductsObject object to a file
+}
+
+function updateUserProduct(request) {
+  const appendUserId = Number(request.params.userId);
+  const appendProductId = Number(request.params.productId);
+  const savedUsers = getData(mockDataPath.users);
+  const appendProduct = request.body;
+
+  const newProductObject = (savedUsers.users[appendUserId].products[
+    appendProductId
+  ] = {
+    ...appendProduct,
+  });
+
+  // TODO write a newProductObject object to a file
+}
+
+function putHttpRoute(
+  app,
+  route,
+  method,
+  mockData = null,
+  resProcessCallback = null,
+  sendMessage
+) {
   // TODO configure cors for a personal server
   const corsOptions = {
     origin: "http://localhost:3000/",
@@ -29,12 +74,16 @@ function putHttpRoute(app, route, method, mockData = null) {
     optionsSuccessStatus: 200,
   };
 
+  app.use(bodyParser.json());
+
   switch (method) {
     case "post":
-      app.post(route, cors(), (req, res) => {
-        // TODO give the updated user object
-        // TODO change file with users
-        res.send(`${route}, ${req.params.id}`);
+      app.post(route, upload.array(), cors(), (req, res) => {
+        if (resProcessCallback) {
+          resProcessCallback(req);
+        }
+
+        res.send(sendMessage);
       });
 
     case "get":
@@ -52,6 +101,22 @@ function serverListener(app, port) {
 
 putHttpRoute(app, routes.users, "get", getData(mockDataPath.users));
 
-putHttpRoute(app, routes.user, "post");
+putHttpRoute(
+  app,
+  routes.userProduct,
+  "post",
+  null,
+  updateUserProduct,
+  "product update success"
+);
+
+putHttpRoute(
+  app,
+  routes.userProducts,
+  "post",
+  null,
+  addUserProduct,
+  "product add success"
+);
 
 serverListener(app, port);
